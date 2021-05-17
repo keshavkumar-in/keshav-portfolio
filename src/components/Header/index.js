a051ee816687668cf6c58c39c671d9a10c6a3bde
Keshav Kumar <kkeshav414@outlook.com>
2021-05-17 10:16:03 +0530

    feat: added header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { Link } from "gatsby"
+import "./Header.scss"
+
+const Header = ({ navbarOpen, setNavbarOpen }) => {
+  const handleToggle = () => {
+    if (navbarOpen === false) {
+      setNavbarOpen(!navbarOpen)
+    } else {
+      setNavbarOpen(false)
+    }
+  }
+
+  return (
+    <header className={`header ${navbarOpen ? "active" : ""}`}>
+      <div className="container">
+        <div className="row flex-end">
+          <button
+            type="button"
+            aria-label="Menu Button"
+            className="nav-toggler"
+            onClick={handleToggle}
+          >
+            <span></span>
+          </button>
+          <nav className={`nav ${navbarOpen ? "showMenu" : ""}`}>
+            <div className="nav-inner">
+              <ul>
+                <li>
+                  <Link to="/" className="nav-item">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about" className="nav-item">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/portfolio" className="nav-item">
+                    Portfolio
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/contact" className="nav-item">
+                    Contact
+                  </Link>
+                </li>
+                <li>
+                  <a
+                    href="https://blog.keshavkumar.in/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="nav-item"
+                  >
+                    Blog
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
