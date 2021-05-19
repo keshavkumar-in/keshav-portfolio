934aaa56833f5f8ad16520759e3d374bbfaf335b
Keshav Kumar <kkeshav414@outlook.com>
2021-05-20 01:40:57 +0530

    feat: add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/Layout"
+import Seo from "../components/seo"
+import Contact from "../components/Contact"
+
+const contact = () => {
+  return (
+    <Layout>
+      <Seo title="Contact Me" />
+      <Contact />
+    </Layout>
+  )
+}
+
+export default contact
